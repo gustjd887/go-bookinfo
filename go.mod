c9ea584f9defd39fa12e6d3419022679eb7f72eb
gustjd887 <gustjd887@gmail.com>
2023-03-01 11:51:26 +0900

    productpage에서 json 표시까지 완료

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/gustjd887/go-bookinfo
+
+go 1.20
---
+++ reviews/v3/review3.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+type ratings []struct {
+	Id   int `json: "Id"`
+	Star int `json: "Star`
+}
+
+type reviewes struct {
+	Id       int    `json: "Id"`
+	Star     int    `json: "Star`
+	Reviewer string `json: "Reviewer"`
+	Review   string `json: "Review"`
+	Color    string `json: "color"`
+}
+
+func main() {
+	resp, err := http.Get("http://localhost:8000/rating")
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%s\n", string(data))
+
+	reviewer1 := reviewes{
+		Id:       1,
+		Star:     0,
+		Reviewer: "Reviewer1",
+		Review:   "An extremely entertaining play by Shakespeare. The slapstick humour is refreshing!",
+		Color:    "red",
+	}
+	reviewer2 := reviewes{
+		Id:       2,
+		Star:     0,
+		Reviewer: "Reviewer2",
+		Review:   "Absolutely fun and entertaining. The play lacks thematic depth when compared to other plays by Shakespeare.",
+		Color:    "red",
+	}
+
+	var r ratings
+	json.Unmarshal(data, &r)
+
+	for _, v := range r {
+		if v.Id == 1 {
+			reviewer1.Star = v.Star
+		} else if v.Id == 2 {
+			reviewer2.Star = v.Star
+		}
+	}
+
+	reviewer := []reviewes{reviewer1, reviewer2}
+	bs, err := json.Marshal(reviewer)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	http.HandleFunc("/review", func(w http.ResponseWriter, r *http.Request) {
+		w.Write(bs)
+	})
+	http.ListenAndServe(":8001", nil)
+
+}
