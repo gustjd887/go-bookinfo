ba2a380e70aaa515ea3f46a85df203e4f69df2f0
gustjd887 <gustjd887@gmail.com>
2023-03-04 12:12:02 +0900

    db connect 및 select 확인

--- ratings/ratings.go
+++ ratings/ratings.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"net/http"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	DB_USER     = "postgres"
+	DB_PASSWORD = "postgres"
+	DB_NAME     = "bookinfo"
 )
 
 type ratings struct {
@@ -12,6 +21,36 @@ type ratings struct {
 }
 
 func main() {
+	dbinfo := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable", DB_USER, DB_PASSWORD, DB_NAME)
+	db, err := sql.Open("postgres", dbinfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	rows, err := db.Query("SELECT * FROM rating")
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var id int
+		var star int
+		err := rows.Scan(&id, &star)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println(id, star)
+	}
+
+	// rows, err := db.Query("SELECT name, age FROM test")
+	// if err != nil {
+	//     panic(err)
+	// }
+	// defer rows.Close()
+
 	reviewer1 := ratings{
 		Id:   1,
 		Star: 5,
